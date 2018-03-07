f87fcbdb77e491bdd2f5d6acd6ce012c89cf2f20
[jianghongjohn] <jianghongjohn@163.com>
2018-03-07 17:17:35 +0800

    mine

--- config.js
+++ config.js
@@ -5,10 +5,9 @@
 // 此处主机域名是腾讯云解决方案分配的域名
 // 小程序后台服务解决方案：https://www.qcloud.com/solution/la
 
-//var host = "14592619.qcloud.la"
+var host = "14592619.qcloud.la"
 
 var config = {
-
   // 下面的地址配合云端 Server 工作
   host,
 
