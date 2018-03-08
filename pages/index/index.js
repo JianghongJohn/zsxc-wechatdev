69ce5e2ae94298ebd34fd8588d5f3c5f3319e1e0
[jianghongjohn] <jianghongjohn@163.com>
2018-03-08 10:10:43 +0800

    完成登录和查询接口，测试通过

--- pages/index/index.js
+++ pages/index/index.js
@@ -21,8 +21,13 @@ Page({
     results:[]
   },
   onLoad: function () {
-
+    //进入自动登录一次，避免登陆过期
     config.login();
+    // config.queryForBlack("jianghong","342725196409052010",function(data){
+      
+    // })
+
+
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
