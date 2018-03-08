5eb471e2f22ba06d6925e68d72d362d47ac42dc3
zhongzikuli <hgb102xlg@126.com>
2018-03-08 10:29:39 +0800

    1

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,4 @@
-const config = require('../../utils/api.js');
+let dataRequest = require('../../utils/dataRequest.js');
 
 const app = getApp();
 Page({
@@ -11,26 +11,26 @@ Page({
       label:"姓名",
       placeholder:"请输入姓名",
       type:"text",
-      class:"name"
+      class:"name",
+      maxlength: 10
     },{
       label: "身份证号",
       placeholder: "请输入身份证号",
       type: "idcard",
-      class: "id"
+      class: "id",
+      maxlength:18
     }],
-    results:[]
+    results:[],
+    resultShow:false,
+    hiddenLoading:true
   },
   onLoad: function () {
-
-    config.login();
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
     } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
         this.setData({
           userInfo: res.userInfo,
@@ -46,16 +46,56 @@ Page({
             userInfo: res.userInfo,
             hasUserInfo: true
           })
+        },
+        fail: res=> {
+          this.setData({
+            getUserInfoFail: true
+          })
         }
       })
     }
   },
-  getUserInfo: function (e) {
-    console.log(e)
+  getUserInfo (e) {
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+  },
+  formSubmit (e){
+    if (e.detail.value.name.length == 0 ) {
+      wx.showToast({
+        title:'姓名不能为空',
+        icon:"none",
+        duration:1500
+      })
+    } else if (e.detail.value.id.length != 15 && e.detail.value.id.length != 18){
+      wx.showToast({
+        title: '身份证号格式不对',
+        icon: "none",
+        duration: 1500
+      })
+    }else{
+      this.setData({
+        hiddenLoading: !this.data.hiddenLoading
+      })
+      var formData = e.detail.value;
+      wx.request({
+        url: '',
+        data: formData,
+        header: {
+          'Content-Type': 'application/json'
+        },
+        success: function (res) {
+          if(res){
+            this.setData({
+              hiddenLoading: !this.data.hiddenLoading,
+              resultShow: !this.data.resultShow,
+              results:res.rows
+            })
+          }
+        }
+      })  
+    }
   }
 })
